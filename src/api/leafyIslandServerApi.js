ae7a0f39baec86eb308310ba6dede05742af033a
Gsangi <sanskar6053@gmail.com>
2021-05-14 14:20:20 +0530

    Test

--- src/api/leafyIslandServerApi.js
+++ src/api/leafyIslandServerApi.js
@@ -1,12 +1,5 @@
-import axios from "axios"
-
-const {
-  NODE_ENV,
-  LEAFY_ISLAND_SERVER_BASE_URL,
-  REACT_APP_LEAFY_ISLAND_SERVER_BASE_URL,
-} = process.env
+import axios from 'axios'
 
 export default axios.create({
-  baseURL:
-    NODE_ENV === "dev" ? REACT_APP_LEAFY_ISLAND_SERVER_BASE_URL : LEAFY_ISLAND_SERVER_BASE_URL,
+  baseURL: process.env.REACT_APP_LEAFY_ISLAND_SERVER_BASE_URL
 })
